111761021c881f9ea49fd166bb057683f0d54218
NanoPedraza1996 <isaacpedraza1996@gmail.com>
2024-06-09 17:41:47 -0300

    Algunas Cosas Que Faltaron.

---
+++ DxY_AppSuplementos/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DxY_AppSuplementos.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ DxY_AppSuplementos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DxY_AppSuplementos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
